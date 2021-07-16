c02bbd6a116cab666cd6d53c6d11a2007162fb88
convin305 <convin305@gmail.com>
2021-07-17 02:24:48 +0900

    Post클래스 만들고, 객체 생성

---
+++ hw_project/project_02.py
@@ -0,0 +1,17 @@
+class Post:
+    id = ''
+    title = ''
+    author = ''
+    content = ''
+
+post = Post()
+post.id = '1'
+post.title = 'first_post'
+post.author = 'sumin'
+post.content = 'Is it Working?'
+
+print(post)
+print("id -> ", post.id)
+print("title -> ", post.title)
+print("author -> ", post.author)
+print("content -> ", post.content)
